0f1f140c5c55c33993154b0a0f755f63d044f313
devbytarun <devbytarun@gmail.com>
2026-03-18 23:08:36 +0530

    Day 3: learned and practiced loops, started objects

---
+++ Web Devops/Practice Questions/Que22.js
@@ -0,0 +1,33 @@
+let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+let num = 2;
+
+for (i = 1; i <= arr.length; i++) {
+  if (arr.length[i] == num);
+  arr.splice(i, 1);
+}
+console.log(arr);
+
+let num1 = 287152;
+let count = 0;
+
+while (num1 > 0) {
+  num1 = Math.floor(num1 / 10);
+  count++;
+}
+console.log(count);
+
+let n = 5;
+let factorial = 1;
+for (let i = 1; i <= n; i++) {
+  factorial *= i;
+}
+console.log(factorial);
+
+letarr = [2, 5, 10, 4, 2, 7, 1, 9];
+letlargest = 0;
+for (leti = 0; i <= arr.length; i++) {
+  if (largest < arr[i]) {
+    largest = arr[i];
+  }
+}
+console.log(largest);
