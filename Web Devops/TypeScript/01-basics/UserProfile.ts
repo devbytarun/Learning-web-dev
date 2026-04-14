81af99e058027a01fbe5a8b46e8f60516587b442
devbytarun <devbytarun@gmail.com>
2026-04-14 14:50:08 +0530

    Day 20: Started Learning TypeScript

---
+++ Web Devops/TypeScript/01-basics/UserProfile.ts
@@ -0,0 +1,8 @@
+let fullName: string = "Tarun";
+let age: number = 19;
+let isPremium: boolean = false;
+let score: number = 95;
+let city: string = "New";
+
+console.log(`Name: ${fullName}, Age: ${age}, City: ${city}, Premium: ${isPremium}, Score: ${score}`);
+export {};
---
+++ Web Devops/TypeScript/01-basics/index.ts
@@ -0,0 +1,25 @@
+let city: string = "jaipur";
+let pin: number = 302001;
+let isCapital: boolean = true;
+
+let studentName: string = "tarun";
+let sem: number = 2;
+let hasbacklog: boolean = false;
+
+let appName:string = "DevByTarun";
+let version: number= 1.0;
+let isLive: boolean= false;
+
+
+
+console.log(appName);
+console.log(version);
+console.log(isLive);
+console.log(studentName);
+console.log(sem);
+console.log(hasbacklog);
+console.log(city);
+console.log(pin);
+console.log(isCapital);
+
+export {};
